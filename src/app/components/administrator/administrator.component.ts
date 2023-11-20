44bab651688a1d2253b5870754c0f1914a2b2cc1
JheysonJhair <jhair2003unamba@gmail.com>
2023-11-20 12:32:45 -0500

    Update: Refactor view and ts for sale

--- src/app/components/administrator/administrator.component.ts
+++ src/app/components/administrator/administrator.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute } from '@angular/router';
 import { dtoOpening } from 'src/app/interfaces/opening';
-import { dtoStudent } from 'src/app/interfaces/student';
 import { AdministratorService } from 'src/app/services/administrator.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ImageDialogComponent } from '../image-dialog/image-dialog.component';
@@ -15,14 +14,12 @@ import { dtoSaleDetail } from 'src/app/interfaces/saleDetail';
   styleUrls: ['./administrator.component.css'],
 })
 export class AdministratorComponent {
-  idPasar: string;
-  stateFinal: boolean;
+  id: string;
 
   admin: dtoAdministrator[] | undefined;
   inputValue: number = 90;
   opening: dtoOpening[] | undefined;
 
-  dtoStudent: dtoStudent | undefined;
   listSaleDetail: dtoSaleDetail[] = [];
 
   constructor(
@@ -31,12 +28,27 @@ export class AdministratorComponent {
     private aRoute: ActivatedRoute,
     private toastr: ToastrService
   ) {
-    this.idPasar = this.aRoute.snapshot.paramMap.get('id')!;
+    this.id = this.aRoute.snapshot.paramMap.get('id')!;
   }
   ngOnInit(): void {
     this.getOpening();
     this.getAdmin();
     this.getSale();
+
+  }
+  //------------------------------------------------------GET -ADMIN - OPENING- VOUCHERS
+  getAdmin() {
+    this._administratorService.getAdmin(this.id).subscribe((data) => {
+      this.admin = data;
+    });
+  }
+
+  getOpening() {
+    this._administratorService
+      .getOpening('3fb8ce38-50b9-4ca8-873e-bbb0c5f4d196')
+      .subscribe((data) => {
+        this.opening = data;
+      });
   }
   getSale() {
     this._administratorService.getSaleDetail().subscribe(
@@ -49,13 +61,15 @@ export class AdministratorComponent {
       }
     );
   }
+
+  //------------------------------------------------------DELETE VOUCHER
   deleteSale(id: any) {
     this._administratorService.deleteSaleDetail(id).subscribe(
       (data) => {
         this.getSale();
         this.toastr.error(
-          'El registro fue eliminado con exito',
-          'Registro eliminado!'
+          'Estudiante no admitido',
+          'Eliminado!'
         );
       },
       (error) => {
@@ -64,11 +78,8 @@ export class AdministratorComponent {
       }
     );
   }
-  getAdmin() {
-    this._administratorService.getAdmin(this.idPasar).subscribe((data) => {
-      this.admin = data;
-    });
-  }
+
+  //------------------------------------------------------ SEE VOUCHER
   openImageDialog(event: Event, imagen: any) {
     event.preventDefault();
     this.dialog.open(ImageDialogComponent, {
@@ -77,7 +88,8 @@ export class AdministratorComponent {
     });
   }
 
-  changeValue(id: any) {
+  //------------------------------------------------------ ADMITIR VOUCHER
+  admitirSale(id: any) {
     this.inputValue -= 1;
     let formData = new FormData();
     formData.append('id', id);
@@ -88,15 +100,8 @@ export class AdministratorComponent {
     });
   }
 
-  getOpening() {
-    this._administratorService
-      .getOpening('b454687f-048d-4c02-8255-885b52c33633')
-      .subscribe((data) => {
-        this.opening = data;
-      });
-  }
-  updateState() {
-    this.stateFinal = true;
+  //------------------------------------------------------ CHANGE OPNNING
+  updateStateApertura() {
 
     let formData = new FormData();
     formData.append('dtoOpening.idOpening', '' + this.opening![0].idOpening);
@@ -105,12 +110,8 @@ export class AdministratorComponent {
       '' + this.opening![0].priorityQuantity
     );
     formData.append('dtoOpening.quantity', '' + this.opening![0].quantity);
-    formData.append('dtoOpening.openState', '' + this.stateFinal);
-    formData.append(
-      'dtoOpening.idPeriod',
-      '101b3361-0aac-410b-9bf4-f9cd00a95f23'
-    );
-    location.reload();
+    formData.append('dtoOpening.openState', '' +true);
+    formData.append('dtoOpening.idPeriod',''+ this.opening![0].idPeriod);
     this._administratorService.updateOpening(formData).subscribe(
       (data) => {
         this.toastr.info('Mensaje', 'Apertura!');
@@ -120,9 +121,11 @@ export class AdministratorComponent {
         console.log(error);
       }
     );
+    setTimeout(function() {
+      location.reload();
+    }, 2000);
   }
-  updateState2() {
-    this.stateFinal = false;
+  updateStateCierre() {
 
     let formData = new FormData();
     formData.append('dtoOpening.idOpening', '' + this.opening![0].idOpening);
@@ -131,20 +134,19 @@ export class AdministratorComponent {
       '' + this.opening![0].priorityQuantity
     );
     formData.append('dtoOpening.quantity', '' + this.opening![0].quantity);
-    formData.append('dtoOpening.openState', '' + this.stateFinal);
-    formData.append(
-      'dtoOpening.idPeriod',
-      '101b3361-0aac-410b-9bf4-f9cd00a95f23'
-    );
-    location.reload();
+    formData.append('dtoOpening.openState', '' + false);
+    formData.append('dtoOpening.idPeriod',''+ this.opening![0].idPeriod);
     this._administratorService.updateOpening(formData).subscribe(
       (data) => {
-        this.toastr.info('Mensaje', 'Apertura!');
+        this.toastr.info('Mensaje', 'Cierre!');
       },
       (error) => {
         this.toastr.error('Opss ocurrio un error', 'Error');
         console.log(error);
       }
     );
+    setTimeout(function() {
+      location.reload();
+    }, 2000);
   }
 }
